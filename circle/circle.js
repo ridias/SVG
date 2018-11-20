2a1f36093d0aaf8f8db6855258890fa60256518f
ricaldo <ricardodias_5@outlook.es>
2018-11-20 11:27:03 +0100

    some changes from the previous commit

---
+++ circle/circle.js
@@ -0,0 +1,7 @@
+let circle = document.getElementById("idCircle")
+
+circle.addEventListener("click", (event) =>{
+    console.log(event.target)
+    //event.target.setAttribute("x", "10000")
+    //console.log(event.target.attributes)
+})
